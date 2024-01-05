3b0102c05a3ec93fc3df4028ce3339332a201eeb
ciaranmckenna <hello@ciaranmckenna.com>
2024-01-05 11:58:04 +0000

    Create entity classes

---
+++ src/main/java/com/ciaranmckenna/readinglistapp/repository/entity/Book.java
@@ -0,0 +1,25 @@
+package com.ciaranmckenna.readinglistapp.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "book")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "title")
+    private String authorId;
+    @Column(name = "author_id")
+    private String lastName;
+    @Column(name = "rating")
+    private String rating;
+}
---
+++ src/main/java/com/ciaranmckenna/readinglistapp/repository/entity/User.java
@@ -0,0 +1,25 @@
+package com.ciaranmckenna.readinglistapp.repository.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
