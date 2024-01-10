bb8caf1fd8d37c5efe1e18ed85066c318733a214
ciaranmckenna <hello@ciaranmckenna.com>
2024-01-10 16:10:34 +0000

    copy of db script

---
+++ sql-scripts/Reading-List-App-05-COPY.sql
@@ -0,0 +1,82 @@
+DROP SCHEMA IF EXISTS `Reading-List-App-04`;
+
+CREATE SCHEMA `Reading-List-App-05`;
+
+use `Reading-List-App-05`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `author` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(128) NOT NULL,
+  `last_name` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `book` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(128) NOT NULL,
+  `author_id` int NOT NULL,
+  `rating` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_AUTHOR_idx` (`author_id`),
+  CONSTRAINT `FK_AUTHOR` 
+  FOREIGN KEY (`author_id`) 
+  REFERENCES `author` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `book_author` (
+  `book_id` int NOT NULL,
+  `author_id` int NOT NULL,
+  PRIMARY KEY (`book_id`, `author_id` ),
+  KEY `author_idx` (`author_id`),
+
+  CONSTRAINT `FK_BOOK` FOREIGN KEY (`book_id`)
+  REFERENCES `course` (`id`)
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+
+  CONSTRAINT `FK_AUTHOR_X` FOREIGN KEY (`author_id`)
+  REFERENCES `author` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `review` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `comment` varchar(256) DEFAULT NULL,
+  `book_id` int DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+
+  KEY `FK_BOOK_ID_idx` (`book_id`),
+
+  CONSTRAINT `FK_BOOK_REVIEW` 
+  FOREIGN KEY (`book_id`) 
+  REFERENCES `book` (`id`) 
+
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `review_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+
+  KEY `FK_REVIEW_idx` (`review_id`),
+  CONSTRAINT `FK_REVIEW`
+  FOREIGN KEY (`review_id`)
+  REFERENCES `review` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+CREATE TABLE `category` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
