b7d9275cf3f0d493c0cfc12a3dbf732aeabd3aef
ciaranmckenna <hello@ciaranmckenna.com>
2024-02-01 10:16:55 +0000

    WIP author search functionality

--- src/main/java/com/ciaranmckenna/readinglistapp/dao/repository/AuthorRepository.java
+++ src/main/java/com/ciaranmckenna/readinglistapp/dao/repository/AuthorRepository.java
@@ -2,6 +2,12 @@ package com.ciaranmckenna.readinglistapp.dao.repository;
 
 import com.ciaranmckenna.readinglistapp.dao.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
+
+    List<Author> findByFirstNameAndLastNameContainingIgnoreCase(String firstName, String lastName);
 }
