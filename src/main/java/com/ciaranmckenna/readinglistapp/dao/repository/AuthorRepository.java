92d69caf403f687329d5f198a736edb913a04839
ciaranmckenna <hello@ciaranmckenna.com>
2024-02-01 11:18:56 +0000

    WIP custom query for author search functionality

--- src/main/java/com/ciaranmckenna/readinglistapp/dao/repository/AuthorRepository.java
+++ src/main/java/com/ciaranmckenna/readinglistapp/dao/repository/AuthorRepository.java
@@ -2,6 +2,8 @@ package com.ciaranmckenna.readinglistapp.dao.repository;
 
 import com.ciaranmckenna.readinglistapp.dao.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,5 +11,9 @@ import java.util.List;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Integer> {
 
-    List<Author> findByFirstNameAndLastNameContainingIgnoreCase(String firstName, String lastName);
+    //List<Author> findByFirstNameAndLastNameContainingIgnoreCase(String firstName, String lastName);
+
+    @Query("SELECT a FROM Author a WHERE LOWER(a.firstName) LIKE LOWER(CONCAT('%', :firstName, '%')) AND LOWER(a.lastName) LIKE LOWER(CONCAT('%', :lastName, '%'))")
+    List<Author> findByFirstNameAndLastNameContainingIgnoreCase(@Param("firstName") String firstName, @Param("lastName") String lastName);
+
 }
